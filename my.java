035602e58f2fd500a1d6094bcc4d7a85f53cd4d3
kanishkaravichandran <132123428+kanishkaravichandran@users.noreply.github.com>
2023-05-14 23:09:10 -0700

    vallapan

--- my.java
+++
@@ -1,8 +0,0 @@
-public class my{
-    public static void main(String args[]){
-        String name;
-        name="kanishka";
-        System.out.println(name);
-
-    }
-}
--- my1.java
+++ my1.java
@@ -1,7 +1,7 @@
 public class my1{
     public static void main(String args[]){
         String name;
-        name="vallapan";
+        name="raja";
         System.out.println(name);
 
     }
